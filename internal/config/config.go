56425a441be4d35f5af5515a8bb460f98661630b
chihyen <nlsh710599@gmail.com>
2023-02-02 19:33:16 +0800

    add config

---
+++ internal/config/config.go
@@ -0,0 +1,35 @@
+package config
+
+import (
+	"log"
+
+	"github.com/kelseyhightower/envconfig"
+)
+
+type Config struct {
+	Port int `default:"8080" split_words:"true"`
+
+	RpcUrl   string `default:"https://data-seed-prebsc-2-s3.binance.org:8545/" split_words:"true"`
+	WsRpcUrl string `default:"wss://testnet-dex.binance.org/api/" split_words:"true"`
+
+	PostgresHost     string `default:"localhost" split_words:"true"`
+	PostgresPort     int    `default:"5432" split_words:"true"`
+	PostgresDatabase string `default:"postgres" split_words:"true"`
+	PostgresUser     string `default:"postgres" split_words:"true"`
+	PostgresPassword string `default:"docker" split_words:"true"`
+
+	confirmationBlockCount int `default:"20" split_words:"true"`
+}
+
+var config Config
+
+func Get() *Config {
+	return &config
+}
+
+func init() {
+	err := envconfig.Process("", &config)
+	if err != nil {
+		log.Panic(err.Error())
+	}
+}
